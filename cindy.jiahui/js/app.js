20bf96d55727d5c605e28571fd35938f9de92318
username <jiahuizhouny@gmail.com>
2021-03-28 21:34:34 -0700

    module4 to module 6

--- cindy.jiahui/js/app.js
+++ cindy.jiahui/js/app.js
@@ -1,12 +1,60 @@
-
-//Document Ready
+// Document Ready
+/* $(document). ready */
 $(()=>{
 
-	$(document)
+   checkUserId();
 
-	.on("submit","#signin-form", function(e){
-		e.preventDefult();
-		checkSigninForm();
-	})
+
+  $(document)
+
+
+/*Form Submits e is event*/
+  .on("submit","#signin-form", function(e){
+   e.preventDefault();
+   checkSigninForm();
+  })
+
+
+
+/*Anchor Click*/
+ .on("click",".js-logout", function (e){
+   sessionStorage.removeItem('userId');
+   checkUserId();
+ })
+  
+
+
+
+
+   
+   /* DATA ACTIVATE */
+   .on("click","[data-activate]",function(e){
+      let target = $(this).data("activate");
+      $(target).addClass("active");
+   })
+   .on("click","[data-deactivate]",function(e){
+      let target = $(this).data("deactivate");
+      $(target).removeClass("active");
+   })
+   .on("click","[data-toggle]",function(e){
+      let target = $(this).data("toggle");
+      $(target).toggleClass("active");
+   })
+
+
+
+
+
+
+
+
+$("[data-template]").each(function(){
+   let id= $(this).data("template");
+   let template = $(id).html();
+   $(this).html(template);
+})
+
+
+
 
 });
